05e8e02869353e9d271512935461f6aab77e0506
Nikita Koshelev <koshelev.nd@gmail.com>
2019-01-15 17:25:24 +0200

    Admin area customization

--- listings/models.py
+++ listings/models.py
@@ -26,4 +26,4 @@ class Listing(models.Model):
     is_published = models.BooleanField(default=True)
     list_date = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
-        return self.title # Main field to be displayed
+        return self.title 
