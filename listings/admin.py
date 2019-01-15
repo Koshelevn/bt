32bd02900d0891e445cb55efb0a271efcfce1976
Nikita Koshelev <koshelev.nd@gmail.com>
2019-01-15 22:02:15 +0200

    Pagination

--- listings/admin.py
+++ listings/admin.py
@@ -9,4 +9,5 @@ class ListingAdmin(admin.ModelAdmin):
     list_editable = ('is_published', 'price')
     search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
     list_per_page = 25
+    
 admin.site.register(Listing, ListingAdmin)
